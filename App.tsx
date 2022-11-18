5f907cf371cec2df947d3025d2cbebfd405df94e
Yahia <yahia.jabeur@gmail.com>
2022-11-19 00:15:17 +0100

    feat: create add location header

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NativeBaseProvider } from 'native-base';
-import './src/libs/i18next';
 
+import './src/libs/i18next';
+import { RootStackParamList } from './src/navigator/types';
 import AddLocation from './src/screens/AddLocation';
 import Home from './src/screens/Home';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
     <NativeBaseProvider>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="AddLocation" component={AddLocation} />
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+          <Stack.Screen
+            name="AddLocation"
+            component={AddLocation}
+            options={{ headerShown: false }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </NativeBaseProvider>
--- src/screens/AddLocation.tsx
+++ src/screens/AddLocation.tsx
@@ -1,5 +1,5 @@
-import { Ionicons } from '@expo/vector-icons';
-import { Box, Button, Center, Icon, Input } from 'native-base';
+import { Ionicons, AntDesign } from '@expo/vector-icons';
+import { Box, Button, Center, HStack, Icon, Input } from 'native-base';
 import { useTranslation } from 'react-i18next';
 
 import { useCurrentWeather } from '../hooks/use-current-weather';
@@ -25,13 +25,27 @@ export default function AddLocation({ navigation }) {
 
   return (
     <Box flex={1}>
-      <Input
-        variant="outline"
-        mx="3"
-        placeholder={t('enterCity') as string}
-        w="90%"
-        onSubmitEditing={(event) => getCities(event.nativeEvent.text)}
-      />
+      <HStack justifyContent="center" my="2">
+        <Icon
+          my="2"
+          mr="2"
+          size="6"
+          color="black"
+          as={<AntDesign name="arrowleft" />}
+          onPress={() => navigation.navigate('Home')}
+        />
+        <Input
+          placeholder={t('enterCity') as string}
+          w="90%"
+          borderRadius="4"
+          fontSize="14"
+          InputLeftElement={
+            <Icon mt="2" mb="2" ml="2" size="6" color="gray.400" as={<Ionicons name="search" />} />
+          }
+          onSubmitEditing={(event) => getCities(event.nativeEvent.text)}
+        />
+      </HStack>
+
       <Center flex={1}>
         <Button
           leftIcon={<Icon as={Ionicons} name="locate" size="sm" />}
